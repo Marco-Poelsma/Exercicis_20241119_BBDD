3f948e34408aca5c387cb62fa4c635382bf3c07d
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2024-11-19 10:47:28 +0100

    Arreglar carácteres máximos en VARCHARs ejercicio 1 + empezar ejercicio 2

--- s3_c1_ej1.sql
+++ s3_c1_ej1.sql
@@ -9,35 +9,35 @@ CREATE TABLE IF NOT EXISTS Temporada(
 );
 
 CREATE TABLE IF NOT EXISTS Competicion(
-	nombre_competicion VARCHAR(20) NOT NULL,
-    nombre_TV VARCHAR(20) NOT NULL,
+	nombre_competicion VARCHAR(50) NOT NULL,
+    nombre_TV VARCHAR(50) NOT NULL,
     CONSTRAINT PK_Competicion PRIMARY KEY (nombre_competicion)
 );
 
 CREATE TABLE IF NOT EXISTS Federacion(
-	nombre_federacion VARCHAR(20) NOT NULL,
+	nombre_federacion VARCHAR(50) NOT NULL,
     fecha_creacion DATE NOT NULL,
-    nombre_responsable VARCHAR(20),
-    apellido1_responsable VARCHAR(20),
-    apellido2_responsable VARCHAR(20),
+    nombre_responsable VARCHAR(50),
+    apellido1_responsable VARCHAR(50),
+    apellido2_responsable VARCHAR(50),
     CONSTRAINT PK_Federacion PRIMARY KEY (nombre_federacion)
 );
 
 CREATE TABLE IF NOT EXISTS Equipo(
-	nombre_equipo VARCHAR(20) NOT NULL,
-    ciudad VARCHAR(20),
-    nombre_presidente VARCHAR(20),
-    apellido1_presidente VARCHAR(20),
-    apellido2_presidente VARCHAR(20),
-    nombre_federacion VARCHAR(20),
+	nombre_equipo VARCHAR(50) NOT NULL,
+    ciudad VARCHAR(50),
+    nombre_presidente VARCHAR(50),
+    apellido1_presidente VARCHAR(50),
+    apellido2_presidente VARCHAR(50),
+    nombre_federacion VARCHAR(50),
     CONSTRAINT PK_equipo PRIMARY KEY (nombre_equipo),
     CONSTRAINT FK_1_equipo FOREIGN KEY (nombre_federacion) REFERENCES Federacion (nombre_federacion) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Clasificacion(
     id_temporada INTEGER UNSIGNED NOT NULL,
-    nombre_competicion VARCHAR(20) NOT NULL,
-    nombre_equipo VARCHAR(20) NOT NULL,
+    nombre_competicion VARCHAR(50) NOT NULL,
+    nombre_equipo VARCHAR(50) NOT NULL,
     posicion TINYINT UNSIGNED NOT NULL,
     CONSTRAINT PK_Clasificacion PRIMARY KEY (id_temporada, nombre_competicion, nombre_equipo),
     CONSTRAINT FK_1_Clasificacion FOREIGN KEY (id_temporada) REFERENCES Temporada (id_temporada) ON DELETE CASCADE ON UPDATE CASCADE,
